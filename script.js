3a274f1e6ab3209bfff43b724540e26de5c60b43
yah0509221747-bit <yah0509221747@gmail.com>
2026-09-05 13:17:57 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function showLogin() {
+    alert("Login page coming soon!");
+}
+
+function showSignup() {
+    alert("Sign Up page coming soon!");
+}
+
+function continueAsGuest() {
+    alert("You are now browsing as a Guest!");
+}
